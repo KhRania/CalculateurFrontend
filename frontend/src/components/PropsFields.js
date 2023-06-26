535258a57dc22e3306246a520d77bb83a0ebaca5
Hedi Makhlouf <hedi.makhlouf@ensi-uma.tn>
2023-06-26 16:53:30 +0100

    added map and configured it +adress field nom+desc

--- frontend/src/components/PropsFields.js
+++ frontend/src/components/PropsFields.js
@@ -1,12 +1,21 @@
-import * as React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+import mapboxgl from 'mapbox-gl';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 
-export default function PropsFields() {
-  const [projectName, setProjectName] = React.useState('');
-  const [address, setAddress] = React.useState('');
-  const [projectNameTouched, setProjectNameTouched] = React.useState(false);
-  const [addressTouched, setAddressTouched] = React.useState(false);
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiaGVkaS1tYWtobG91ZiIsImEiOiJjbGpjdDJwdXowM3Z2M2RzMnJzMW5id2N1In0.gH1XsrErXO_3cp7ZxirG2g';
+mapboxgl.setRTLTextPlugin(
+  'https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-rtl-text/v0.2.3/mapbox-gl-rtl-text.js',
+  null,
+  true 
+  );
+
+const PropsFields = () => {
+  const [projectName, setProjectName] = useState('');
+  const [address, setAddress] = useState('');
+  const [projectNameTouched, setProjectNameTouched] = useState(false);
+  const [addressTouched, setAddressTouched] = useState(false);
 
   const handleProjectNameChange = (event) => {
     setProjectName(event.target.value);
@@ -27,11 +36,70 @@ export default function PropsFields() {
   const projectNameError = projectNameTouched && projectName === '';
   const addressError = addressTouched && address === '';
 
+  const [coordinates, setCoordinates] = useState([]);
+  const mapContainerRef = useRef(null);
+  const mapRef = useRef(null);
+  const markerRef = useRef(null);
+
+  useEffect(() => {
+    const geocodeAddress = async () => {
+      if (address.trim() !== '') {
+        const response = await fetch(
+          `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=${mapboxgl.accessToken}`
+        );
+        const data = await response.json();
+        const [lng, lat] = data.features[0].center;
+        setCoordinates([lng, lat]);
+      }
+    };
+
+    geocodeAddress();
+  }, [address]);
+
+  useEffect(() => {
+    if (coordinates.length > 0) {
+      const [lng, lat] = coordinates;
+
+      if (!mapRef.current) {
+        mapRef.current = new mapboxgl.Map({
+          container: mapContainerRef.current,
+          style: 'mapbox://styles/mapbox/streets-v11',
+          center: [lng, lat],
+          zoom: 12
+        });
+
+        mapRef.current.on('click', (e) => {
+          const { lngLat } = e;
+          reverseGeocode(lngLat);
+        });
+
+        markerRef.current = new mapboxgl.Marker().setLngLat([lng, lat]).addTo(mapRef.current);
+      } else {
+        mapRef.current.flyTo({
+          center: [lng, lat],
+          zoom: mapRef.current.getZoom() 
+        });
+
+        markerRef.current.setLngLat([lng, lat]);
+      }
+    }
+  }, [coordinates]);
+
+  const reverseGeocode = async (lngLat) => {
+    const response = await fetch(
+      `https://api.mapbox.com/geocoding/v5/mapbox.places/${lngLat.lng},${lngLat.lat}.json?access_token=${mapboxgl.accessToken}`
+    );
+    const data = await response.json();
+    const address = data.features[0].place_name;
+    setAddress(address);
+  };
+
   return (
     <Box
       component="form"
       sx={{
-        '& .MuiTextField-root': { m: 2, width: '30ch' }, marginTop: 2
+        '& .MuiTextField-root': { m: 2, width: '30ch' },
+        marginTop: 2
       }}
       noValidate
       autoComplete="off"
@@ -49,6 +117,9 @@ export default function PropsFields() {
         />
       </div>
       <div>
+        <TextField id="outlined-multiline-flexible" label="Description" multiline />
+      </div>
+      <div>
         <TextField
           required
           id="outlined-required"
@@ -60,13 +131,9 @@ export default function PropsFields() {
           helperText={addressError && 'Address is required'}
         />
       </div>
-      <div>
-        <TextField
-          id="outlined-multiline-flexible"
-          label="Description"
-          multiline
-        />
-      </div>
+      <div id="map" style={{ height: '400px' }} ref={mapContainerRef}></div>
     </Box>
   );
-}
+};
+
+export default PropsFields;
